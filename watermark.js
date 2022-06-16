1b7dc040618df2b44469727d3291295aad4e7149
ozgurguz <104092246+ozgurguz@users.noreply.github.com>
2022-06-16 22:35:28 +0300

    Update and rename watermark.js to src/index.js

--- watermark.js
+++
@@ -1,5 +0,0 @@
-watermark(['/img/11.jpg', '/img/1120x630_Logo_AA.png'])
-  .image(watermark.image.lowerRight())
-  .then(function (img) {
-    document.getElementById('composite-image').appendChild(img);
-  });
